a6c17430b9edd76818f99286bc33dbf37bb1ea9f
fabioalcocer <fabioalcocerdev@gmail.com>
2025-10-04 14:38:30 -0400

    delete envs

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,9 +1,7 @@
 export const environment = {
   production: process.env.NODE_ENV === 'production',
   firebase: {
-    apiKey:
-      process.env.NEXT_PUBLIC_FIREBASE_API_KEY ||
-      'AIzaSyBDi1ghX9l9ManUtSRgM0fwmWEF8e5DnAk',
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain:
       process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN ||
       'propio-bo.firebaseapp.com',
@@ -13,13 +11,8 @@ export const environment = {
       'propio-bo.firebasestorage.app',
     messagingSenderId:
       process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || '237550271849',
-    appId:
-      process.env.NEXT_PUBLIC_FIREBASE_APP_ID ||
-      '1:237550271849:web:832fd353e429da76df11f8',
-    measurementId:
-      process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || 'G-2KWKHVYJM8',
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
   },
-  cloudFunctionsUrl:
-    process.env.NEXT_PUBLIC_CLOUD_FUNCTIONS_URL ||
-    'https://us-central1-propio-bo.cloudfunctions.net',
+  cloudFunctionsUrl: process.env.NEXT_PUBLIC_CLOUD_FUNCTIONS_URL,
 }
